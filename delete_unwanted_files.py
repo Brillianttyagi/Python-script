75e023bbd6e45ca7d712eca84de9fd794bc5d7e7
sathvik <sathvik.palyam@unlearnkids.com>
2020-10-06 11:59:46 +0530

    added print statements

--- delete_unwanted_files.py
+++ delete_unwanted_files.py
@@ -2,15 +2,18 @@ import os
 import glob
 
 PATH = 'G:\courses\[FreeCourseSite.com] Udemy - Python for Data Science and Machine Learning Bootcamp'
-FILE_TYPE = 'VTT'
+FILE_TYPE = 'vtt'
+
+
 def remove(path, typ):
 
     os.chdir(path)
     folders = os.listdir()
     for x in range(len(folders)):
         os.chdir(folders[x])
-
+        print(f'in {folders[x]}')
         for file in glob.glob(f'*.{typ}'):
+            print(f'removing:{file}')
             os.remove(file)
         os.chdir(path)
 
